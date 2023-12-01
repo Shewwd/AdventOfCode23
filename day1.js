c3fc1846991695782d058f4f66be18cbed917568
Jake Shawd <jacobshawd@gmail.com>
2023-12-01 11:22:24 -0600

    day1 a

--- day1.js
+++ day1.js
@@ -1,16 +1,59 @@
-const test_input = ['1abc2', 'pqr3stu8vwx', 'a1b2c3d4e5f', 'treb7uchet'];
+const fs = require('node:fs');
+
+let data;
+try {
+  data = fs.readFileSync('input.txt', 'utf8');
+} catch (err) {
+
+}
+
+const test_input = data.split('\r\n');
 
 let digits = [];
 
-const nums = 
-
-test_input.forEach((str, index) => {
+test_input.map((str, idx) => {
+    let numAdded = 0;
+    let firstNum;
+    let secondNum;
     for(let i = 0; i < str.length; i++){
         const parsed = parseInt(str[i]);
 
-        if(parsed != NaN)
-            digits[index].push(parsed);
+        if(!Number.isNaN(parsed)){
+            if(numAdded === 0){
+                firstNum = parsed;
+                numAdded++;
+            } else {
+                secondNum = parsed;
+            }
+        }
     }
+
+    digits.push({firstNum, secondNum});
 });
 
 console.log(digits);
+
+let sums = [];
+
+digits.map((obj, index) => {
+    let numStr = `${obj.firstNum}`;
+
+    if(obj.secondNum === undefined){
+        numStr += `${obj.firstNum}`;
+    } else {
+        numStr += `${obj.secondNum}`;
+    }
+
+    sums[index] = parseInt(numStr);
+})
+
+console.log(sums);
+
+
+let totalSum = 0;
+
+sums.map(num => {
+    totalSum += parseInt(num);
+});
+
+console.log(totalSum);
